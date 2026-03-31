6da0feca75b3a84e94938855aa4a5397b759868e
kenmochi-blip <kenmochi@smartandsmooth.com>
2026-03-31 16:25:48 +0900

    Add files via upload

---
+++ game-timer/sw.js
@@ -0,0 +1,4 @@
+const CACHE_NAME = 'game-timer-v2';
+const urlsToCache = [ './', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png' ];
+self.addEventListener('install', (event) => { event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))); });
+self.addEventListener('fetch', (event) => { event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request))); });
